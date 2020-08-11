3f01979e292dcf0d7d26ea7b409a0ba04cdac826
Woomi Ban <banwoomi@gmail.com>
2020-08-11 12:10:43 -0400

    Initial Upload

---
+++ test/plot_test.py
@@ -0,0 +1,195 @@
+from pynvt.base import pyNVT
+
+
+if __name__ == '__main__':
+
+    # path_template = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_DukeUNC/Rat_Paxinos_400um_EPI.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_DukeUNC/Rat_Paxinos_400um_Labels_190627.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_DukeUNC/Rat_Paxinos_400um_Labels_190627.label'
+    # path_result = '/Users/woomi_mac/Downloads/TEST_PLOT/'
+    # result_file_nm = 'template_plot.png'
+    # result_legend = 'template_legend.png'
+
+    # path_template = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/02_CAMRI_EarFilling/Rat_CAMRI_400um_EPI_v1.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/02_CAMRI_EarFilling/Rat_CAMRI_Labels_v1.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/02_CAMRI_EarFilling/Rat_CAMRI_Labels_v1.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/20_plot'
+    # result_file_nm = 'template_Rat_CAMRI_400um_EPI_v1_plot.png'
+    # result_legend = 'template_Rat_CAMRI_400um_EPI_v1_legend.png'
+
+    # path_template = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/200_spatial_morrow.nii.gz'
+    # path_over_img = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/ROI_RSC_Morrow_20191011.nii.gz'
+    # path_label = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/ROI_RSC_Morrow_20191011.label'
+    # path_result = '/Users/woomi_mac/Downloads/TEST_PLOT/'
+    # result_file_nm = '200_morrow_plot.png'
+    # result_legend = '200_morrow_legend.png'
+
+    # path_template = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/3D_rat_temp_200um_Duke.nii.gz'
+    # path_over_img = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/seeds.nii.gz'
+    # path_label = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/seeds.label'
+    # path_result = '/Users/woomi_mac/Downloads/TEST_PLOT/'
+    # result_file_nm = 'besheer_plot.png'
+    # result_legend = 'besheer_legend.png'
+
+    # path_template = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/2D_rat_1mm_12slices_200um_at_5AC_181120.nii.gz'
+    # path_over_img = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/septum_rois.nii.gz'
+    # path_label = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/septum_rois.label'
+    # path_result = '/Users/woomi_mac/Downloads/TEST_PLOT/'
+    # result_file_nm = 'dom_plot.png'
+    # result_legend = 'dom_legend.png'
+
+    # AFNI
+    # path_template = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/Rat_Paxinos_400um_EPI.nii.gz'
+    # path_over_img = '/Users/woomi_mac/Downloads/TEST_PLOT/origin/01_3dMVM_output.nii.gz'
+    # path_result = '/Users/woomi_mac/Downloads/TEST_PLOT/'
+    # result_file_nm = 'AFNI_besheer_plot.png'
+    # result_legend = 'AFNI_besheer_legend.png'
+
+    # Besheer & Kash
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_EPI.nii.gz'
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Template.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Labels_BK_200123.nii.gz'
+    # path_label    = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Labels_BK_200123.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Documents/plot_template'
+    # result_file_nm = 'besheer_roi.png'
+    # result_legend = 'besheer_roi_legend.png'
+
+    # Besheer & Kash Seed
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Template.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/seeds_400um_Besheer_20200123.nii.gz'
+    # path_label    = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Labels_BK_200123.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Documents/plot_template'
+    # result_file_nm = 'besheer_seed.png'
+    # result_legend = 'besheer_seed_legend.png'
+
+    # Besheer & Kash Seed AC 05 / ROI
+    # lme_nm = "IG_AC-05"
+    # # lme_nm = "IG_DG-05"
+    # # lme_nm = "IG_INS-05"
+    # # lme_nm = "IG_RhReThal-05"
+    # # lme_nm = "IG_VSept-05"
+    # # lme_nm = "IG_mPFC-05"
+    # # path_template = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Template/Rat_Paxinos_400um_Template.nii.gz'
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Results/UNCCH_CAMRI/703_LME_Mask/Atlas_mask.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Results/UNCCH_CAMRI/703_LME_Mask/' + lme_nm + '.nii.gz'
+    # path_label    = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Results/UNCCH_CAMRI/703_LME_Mask/Atlas_mask.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200107_Besheer_Kash/Documents/plot_template'
+    # result_file_nm = 'besheer_' + lme_nm + '.png'
+    # result_legend = 'besheer_' + lme_nm + '_legend.png'
+
+    # Morrow
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/Rat_CAMRI_400um_EPI_v1.nii.gz'
+    # # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_Morrow_20200522.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_Morrow_20200414.nii.gz'
+    # # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Results/UNCCH_CAMRI/40E_ReHo_Threshold_ClusterMask/Mask_glt_01_Gavage_F.nii.gz'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/Rat_CAMRI_Labels_v1.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_Morrow_20200522.label'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_Morrow_20200414.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_ReHo_glt1_Morrow.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Documents/Plot/'
+    # result_file_nm = 'roi_Nac.png'
+    # result_legend = 'roi_Nac_legend.png'
+
+    # # Donita
+    path_template = '/Users/woomi_mac/01_CORE_Project/20190129_DonitaRobinsonARC/Template/Rat_Paxinos_400um_EPI.nii.gz'
+    path_over_img = '/Users/woomi_mac/01_CORE_Project/20190129_DonitaRobinsonARC/Template/Thal_add/ROI_Robinson_Thal.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/20190129_DonitaRobinsonARC/Template/Thal_add/ROI_Robinson_Thal.label'
+    path_label = '/Users/woomi_mac/01_CORE_Project/20190129_DonitaRobinsonARC/Template/Thal_add/ROI_Robinson_Thal_colorchange_20200730.label'
+    path_result = '/Users/woomi_mac/01_CORE_Project/20190129_DonitaRobinsonARC/Documents/Plot/'
+    result_file_nm = 'roi_colorchanged_Donita.png'
+    result_legend = 'roi_colorchanged_Donita_leg.png'
+
+
+    # Morrow
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/Rat_CAMRI_400um_EPI_v1.nii.gz'
+    # # path_template = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/01_DukeUNC/Rat_Paxinos_400um_EPI.nii.gz'
+    # # path_template = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Processing/UNCCH_CAMRI/040_ApplySpatialNorm-func/sub-MLRA26CLC/sub-MLRA26CLC_task-rs_bold.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/ROI_Morrow.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Template/Rat_CAMRI_Labels_v1.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200115_MorrowHerman/Documents/Plot/Plot_CompareFilling'
+    # result_file_nm = 'Template_curr.png'
+    # fig_nm = "Novel Template(ROI)"
+    # # result_file_nm = 'plot_EV_MLRA26CLC.png'
+    # # fig_nm = "EV-MLRA26CLC"
+    # result_legend = 'roi_legend.png'
+
+    # RobinsonCOC
+    # path_template = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/01_DukeUNC/Rat_Paxinos_400um_EPI.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Results/UNCCH_CAMRI/434_Threshold_MergedClusterMask/Mask_glt_01_SetA-SetB_Tstat.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/01_DukeUNC/Rat_Paxinos_400um_Labels_190627.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Template/ROI_Cluster_A-B.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Documents/Plot/'
+    # result_file_nm = '434_cluser_plot.png'
+    # result_legend = '434_cluser_plot_legend.png'
+
+    # # RobinsonCOC
+    # path_template = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/01_DukeUNC/Rat_Paxinos_400um_EPI.nii.gz'
+    # # path_over_img = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Results/UNCCH_CAMRI/511_3dttest_oneFolder/ttest_01_A-B/07_S2_3dttest_output.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Results/UNCCH_CAMRI/514_Intersection_Atlas/ttest_01_A-B/atlas_07_S2-01.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/template_CAMRI/Rat_Template/01_DukeUNC/Rat_Paxinos_400um_Labels_190627.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Template/ROI_Cluster_A-B.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20190801_DonitaRobinsonCOC/Documents/Plot/'
+    # result_file_nm = '07_S2_atlas.png'
+    # # result_legend = '00_CPu_afni_legend.png'
+
+    # Fulton
+    # path_template = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Template/Rat_CAMRI_400um_EPI_v1.nii.gz'
+    # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Results/UNCCH_CAMRI/40E_ReHo0_Threshold_ClusterMask/Mask_glt_02_Condition_F.nii.gz'
+    # # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Results/UNCCH_CAMRI/41E_ReHo1_Threshold_ClusterMask/Mask_glt_02_Condition_F.nii.gz'
+    # # path_over_img = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Results/UNCCH_CAMRI/42E_ReHo2_Threshold_ClusterMask/Mask_glt_02_Condition_F.nii.gz'
+    # path_label = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Template/Rat_CAMRI_Labels_v2.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Documents/Plot/ReHo/test_ReHo0_label/Mask_glt_02_Condition_F.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Documents/Plot/ReHo/test_ReHo1_label/Mask_glt_02_Condition_F.label'
+    # # path_label = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Documents/Plot/ReHo/test_ReHo2_label/Mask_glt_02_Condition_F.label'
+    # path_result = '/Users/woomi_mac/01_CORE_Project/20200116_Fulton/Documents/Plot/ReHo'
+    # result_file_nm = 'ReHo0_glt02.png'
+    # result_legend = 'ReHo0_legend.png'
+    # # result_file_nm = 'ReHo1_glt02.png'
+    # # result_legend = 'ReHo1_legend.png'
+    # # result_file_nm = 'ReHo2_glt02.png'
+    # # result_legend = 'ReHo2_legend.png'
+
+    nvt = pyNVT(path_template, path_over_img, path_result, result_file_nm,
+                # data_type='UNDER_ONLY',
+                data_type='ATLAS',
+                # data_type='AFNI',
+                # afni_brick_idx=1,
+                # value_range=5,
+                # p_value=0.05,
+                # threshold=0.01,
+                choose_axis='y',
+                # fig_n_column=10,
+                fig_n_column=5,
+                space=1,
+                slice_label='both',
+                # slice_label='slice_coordinate',
+                # slice_label='slice_index',
+                slice_coordinate=29,
+                slice_coordinate_value=-0.36,
+                slice_range=(15, 44),
+                # slice_range=(21, 27),
+                transparent=0.7,
+                dpi=300,
+                # fig_title=fig_nm,
+                path_label=path_label,
+                legend_bool=False,
+                file_legend=result_legend,
+                leg_n_column=10,
+                leg_cell_width=150,
+                # leg_swatch_width=70,
+                intensity_max=0.7,
+                intensity_min=0.1,
+                      )
+
+    # Print
+    nvt.print_avail_brick_afni()
+
+    # LOAD
+    nvt.load_data()
+
+    # Make Figure
+    nvt.make_figure()
+
+    # Make Legend
+    if nvt.legend_bool:
+        nvt.make_legend()
